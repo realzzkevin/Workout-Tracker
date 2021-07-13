e6077c103d968bb9d863886b25e312ae116fa987
zzkevin <realzzkevin@gmail.com>
2021-07-13 13:18:00 -0400

    models created

--- models/workout.js
+++ models/workout.js
@@ -2,41 +2,21 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Enter the workout type"
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter the name of workout"
-    },
-    date: {
+const WorkoutSchema = new Schema({
+    day: {
         type: Date,
-        default: Date.now
-    }, 
-    duration: {
-        type: Number,
+        required: "create date is required"
     },
-    weight: {
-        type: Number,
-    },
-    reps: {
-        type: Number,
-        
-    },
-    sets: {
-        type: Number,
-    },
-    distance: {
-        type: Number
-    }
+    excercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Excercise"
+        }
+    ]
 
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
 
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 //@ts-check
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/Workout.js");
 
 router.get("api/worksouts");
 
